e7fd7ad8a03e770772462037f462adf14540e68a
Janath Ranasinghe <janathmanthila95@gmail.com>
2020-08-06 13:08:40 +0530

    [IMP] Add Color-display component

---
+++ src/components/color-display/color-display.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "bootstrap";
+import './color-display.styles.css';
+
+const ColorDisplay = ({colors}) => (
+    <div className='color-display'>
+        {
+            Object.keys(colors).map(key => (
+                <div className='d-flex flex-column color-detail-container'>
+                    <div className='color-point' style={{border: '4px solid #3788d8',borderColor: colors[key]}}></div>
+                    <div className='color-description'>{key}</div>
+                </div>
+            ))
+        }
+
+    </div>
+)
+
+export default ColorDisplay;
